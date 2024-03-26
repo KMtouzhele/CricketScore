6bce64921eec09849f0e226367b382dfc87daa9b
KMtouzhele <kilmerau@hotmail.com>
2024-03-27 00:48:12 +1100

    Initial commit

---
+++ app/src/main/java/au/edu/utas/kaimol/cricketscore/MainActivity.kt
@@ -0,0 +1,11 @@
+package au.edu.utas.kaimol.cricketscore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
